16a3c0e2ed55810d32b0da9d6ba05d352fa9543a
Alberto Romero <aromeronavia@gmail.com>
2021-12-11 00:03:35 -0600

    added second part of day 6

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -1,12 +1,11 @@
-//
+use std::collections::HashMap;
 
-fn first_part() {
-    let input = [3,4,3,1,2];
-    let mut fishes: Vec<i32> = input.iter().map(|x| *x).collect::<Vec<i32>>();
+fn first_part(input: &[i64]) {
+    let mut fishes: Vec<i64> = input.iter().map(|x| *x).collect();
 
-    let days = 256;
+    let days = 80;
 
-    for day in 0..days {
+    for _day in 0..days {
         for index in 0..fishes.len() {
             if fishes[index] == 0 {
                 fishes[index] = 6;
@@ -20,57 +19,43 @@ fn first_part() {
     println!("{}", fishes.len());
 }
 
-fn second_part() {
-    let input = [6];
-    let mut fishes: Vec<i32> = input.iter().map(|x| *x).collect::<Vec<i32>>();
-    let days = 18;
+fn second_part(input: &[i64]) {
+    let mut fishes_per_day: HashMap<i64, i64> = HashMap::new();
+    let days = 256;
 
-    // 18 / 7 = 2         12 / 9 = 1
-    // 11 / 7 = 1         3 / 7 = 0
-    // 4 / 7 = 0
+    let mut result: i64 = input.len() as i64;
 
-    let mut days_remainings: Vec<i32> = Vec::new();
-    let mut tmp_days = days;
-
-    days_remainings.push(days);
-    let mut day_iterator = days;
-    loop {
-        if day_iterator - 7 < 0 {
-            break;
-        }
-
-        days_remainings.push(day_iterator - 7);
-        day_iterator -= 7;
+    for i in 0..=8 {
+        fishes_per_day.insert(i, 0);
     }
 
-    println!("{:?}", days_remainings);
-
-    let mut acc = 0;
-
-    for index in 0..input.len() {
-        for day_iterator in 0..days_remainings.len() {
-            let mut day = days_remainings[day_iterator];
-            let mut i = 0;
-
-            loop {
-                if i == 0 {
-                    acc += days_remainings[i] - input[index] / 7;
-                    day -= 7;
-                }
-                else {
-                    acc += days_remainings[i] / 7;
-                    day -= 9;
-                }
-            }
-        }
+    for i in input {
+        fishes_per_day.insert(*i, fishes_per_day[&i] + 1);
     }
 
-    println!("Len: {}", acc);
+    for _day in 0..days {
+        let mut tmp: HashMap<i64, i64> = HashMap::new();
+        for i in 0..=8 {
+            tmp.insert(i, 0);
+        }
+
+        result += fishes_per_day[&0];
+        tmp.insert(6, fishes_per_day[&0]);
+        tmp.insert(8, fishes_per_day[&0]);
+
+        for i in 1..=8 {
+            tmp.insert(i - 1, tmp[&(i-1)] + fishes_per_day[&i]);
+        }
+
+        fishes_per_day = tmp;
+    }
+
+    println!("Result: {}", result);
 }
 
 fn main() {
-    //let input = [1,4,3,3,1,3,1,1,1,2,1,1,1,4,4,1,5,5,3,1,3,5,2,1,5,2,4,1,4,5,4,1,5,1,5,5,1,1,1,4,1,5,1,1,1,1,1,4,1,2,5,1,4,1,2,1,1,5,1,1,1,1,4,1,5,1,1,2,1,4,5,1,2,1,2,2,1,1,1,1,1,5,5,3,1,1,1,1,1,4,2,4,1,2,1,4,2,3,1,4,5,3,3,2,1,1,5,4,1,1,1,2,1,1,5,4,5,1,3,1,1,1,1,1,1,2,1,3,1,2,1,1,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,1,4,5,1,3,1,4,4,2,3,4,1,1,1,5,1,1,1,4,1,5,4,3,1,5,1,1,1,1,1,5,4,1,1,1,4,3,1,3,3,1,3,2,1,1,3,1,1,4,5,1,1,1,1,1,3,1,4,1,3,1,5,4,5,1,1,5,1,1,4,1,1,1,3,1,1,4,2,3,1,1,1,1,2,4,1,1,1,1,1,2,3,1,5,5,1,4,1,1,1,1,3,3,1,4,1,2,1,3,1,1,1,3,2,2,1,5,1,1,3,2,1,1,5,1,1,1,1,1,1,1,1,1,1,2,5,1,1,1,1,3,1,1,1,1,1,1,1,1,5,5,1];
-    // first_part();
-    second_part();
+    let input = [1,4,3,3,1,3,1,1,1,2,1,1,1,4,4,1,5,5,3,1,3,5,2,1,5,2,4,1,4,5,4,1,5,1,5,5,1,1,1,4,1,5,1,1,1,1,1,4,1,2,5,1,4,1,2,1,1,5,1,1,1,1,4,1,5,1,1,2,1,4,5,1,2,1,2,2,1,1,1,1,1,5,5,3,1,1,1,1,1,4,2,4,1,2,1,4,2,3,1,4,5,3,3,2,1,1,5,4,1,1,1,2,1,1,5,4,5,1,3,1,1,1,1,1,1,2,1,3,1,2,1,1,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,1,4,5,1,3,1,4,4,2,3,4,1,1,1,5,1,1,1,4,1,5,4,3,1,5,1,1,1,1,1,5,4,1,1,1,4,3,1,3,3,1,3,2,1,1,3,1,1,4,5,1,1,1,1,1,3,1,4,1,3,1,5,4,5,1,1,5,1,1,4,1,1,1,3,1,1,4,2,3,1,1,1,1,2,4,1,1,1,1,1,2,3,1,5,5,1,4,1,1,1,1,3,3,1,4,1,2,1,3,1,1,1,3,2,2,1,5,1,1,3,2,1,1,5,1,1,1,1,1,1,1,1,1,1,2,5,1,1,1,1,3,1,1,1,1,1,1,1,1,5,5,1].map(|x| x as i64);
+    first_part(&input);
+    second_part(&input);
 }
 
